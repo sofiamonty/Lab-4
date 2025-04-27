e98e9d5b64de653d4500ad2495d6b7b0e96079fe
sofiamonty <sofia.montaguti@metropolia.fi>
2025-04-27 11:05:20 +0300

    Add files via upload

---
+++ task4-A.cpp
@@ -0,0 +1,20 @@
+//Task 4 part 1 copied from the PDF - cin.ignore(); is used
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int age;
+    string name;
+    
+    cout<<"Enter your age: ";
+    cin>>age;
+    cin.ignore();
+
+    cout<<"Enter your full name: ";
+    getline(cin, name);
+
+    cout<<"Age: "<<age<<", Name: "<<name<<endl;
+
+    return 0;
+}
